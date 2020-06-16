89a13d548b276a5cada089271bf668f40314759a
Solaiman Shadin <solaimanshadin@gmail.com>
2020-06-16 19:34:33 +0600

    Ready for deployment

---
+++ build/precache-manifest.a6feb2d2aa410727052b45cdca15f6ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bcb1ecaa281adb3db62776ef1dc5d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a28d3455baeb5cbee89",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "609cbd50aa1073ae82da",
+    "url": "/static/js/2.209645d1.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.209645d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a28d3455baeb5cbee89",
+    "url": "/static/js/main.26975851.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const app = express();
 const cors = require("cors")
-app.use(cors())
+app.use(cors());
+app.use(express.static('public'))
+
 app.use("/graphql", graphqlHTTP({
     schema,
     graphiql:true
 }))
+
+app.get('*', (req,res) => {
+  res.sendFile(__dirname, 'build', 'index.html')
+})
 
 const port = process.env.PORT || 1010;
 app.listen(port , () => console.log("Listining for port", port))
