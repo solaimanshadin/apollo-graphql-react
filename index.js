a1c17c06d7cebaef629af39a5113eb60e49f29c5
Solaiman Shadin <solaimanshadin@gmail.com>
2020-06-16 14:14:14 +0600

    CORS installed

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const app = express();
-
+const cors = require("cors")
+app.use(cors())
 app.use("/graphql", graphqlHTTP({
     schema,
     graphiql:true
